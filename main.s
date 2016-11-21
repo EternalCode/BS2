e929eb727e3eaed4a59ed73cab9ccbda41a91d85
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-21 18:18:18 -0500

    Hook for exit button to enable battle start
    Just for testing purposes, this is the fastest way

--- main.s
+++ main.s
@@ -2,7 +2,7 @@
         .thumb
         .open "roms/BPRE0.gba","build/multi.gba", 0x08000000
 
-        //.include "patches/"
+        .include "patches/menutest.s"
 
         .org 0x08800000
         .importobj "build/linked.o"
