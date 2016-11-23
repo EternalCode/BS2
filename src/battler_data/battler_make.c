f0ec58411f03d97881692c2383445028b15de3bd
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-22 20:39:34 -0500

    Increase pkmn data
    fixed some errors in compilation too

--- src/battler_data/battler_make.c
+++ src/battler_data/battler_make.c
@@ -1,18 +1,20 @@
 #include <pokeagb/pokeagb.h>
 #include "battler_data.h"
 
+static struct battler* opponent = (struct battler*)0x202024C;
+static struct battler* player = (struct battler*)0x2024284;
 
 void battler_make(u16 species, u8 level, u8 side) {
-    struct battler *this_pkmn;
+    struct battler* this_pkmn;
     switch (side) {
-        case 0:
+        case 1:
         {
-            this_pkmn = (struct battler*)&opponent[side];
+            this_pkmn = (struct battler*)&opponent[side - 1];
             break;
         }
         default:
         {
-            this_pkmn = player;
+            this_pkmn = (struct battler*)player;
             break;
         }
     };
@@ -23,9 +25,8 @@ void battler_make(u16 species, u8 level, u8 side) {
     struct PokemonBaseStat species_stats = pokemon_base_stats[species];
     
     // hp
-    this_pkmn->current_hp = ((species_stats.hp) >> 3) * level;
-    this_pkmn->total_hp = this_pkmn->current_hp;
-    
+    this_pkmn->total_hp = ((species_stats.hp) >> 3) * level;
+    this_pkmn->current_hp = this_pkmn->total_hp;
     //atk
     this_pkmn->atk = (species_stats.atk >> 3) * level;
     this_pkmn->spatk = (species_stats.spatk >> 3) *level;
@@ -38,7 +39,7 @@ void battler_make(u16 species, u8 level, u8 side) {
     this_pkmn->spd = (species_stats.spd >> 4) * level;
     
     // misc data
-    memcpy((void *)&this_pkmn->name[0], (void*)&battler_names[species], 15);
+    memcpy((void *)this_pkmn->name, (void*)&battler_names[species], 15);
     this_pkmn->species = species;    
     this_pkmn->level = level;
     
