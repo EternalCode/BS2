2e086140c34fe89f1bb165d371c5abcb5fe3b13d
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-26 16:27:59 -0500

    Switched back to PR images andded razorleaf frames

--- src/portraits.h
+++ src/portraits.h
@@ -42,136 +42,6 @@
 #include "../generated/images/portraits/face037.h"
 #include "../generated/images/portraits/face038.h"
 #include "../generated/images/portraits/face039.h"
-#include "../generated/images/portraits/face040.h"
-#include "../generated/images/portraits/face041.h"
-#include "../generated/images/portraits/face042.h"
-#include "../generated/images/portraits/face043.h"
-#include "../generated/images/portraits/face044.h"
-#include "../generated/images/portraits/face045.h"
-#include "../generated/images/portraits/face046.h"
-#include "../generated/images/portraits/face047.h"
-#include "../generated/images/portraits/face048.h"
-#include "../generated/images/portraits/face049.h"
-#include "../generated/images/portraits/face050.h"
-#include "../generated/images/portraits/face051.h"
-#include "../generated/images/portraits/face052.h"
-#include "../generated/images/portraits/face053.h"
-#include "../generated/images/portraits/face054.h"
-#include "../generated/images/portraits/face055.h"
-#include "../generated/images/portraits/face056.h"
-#include "../generated/images/portraits/face057.h"
-#include "../generated/images/portraits/face058.h"
-#include "../generated/images/portraits/face059.h"
-#include "../generated/images/portraits/face060.h"
-#include "../generated/images/portraits/face061.h"
-#include "../generated/images/portraits/face062.h"
-#include "../generated/images/portraits/face063.h"
-#include "../generated/images/portraits/face064.h"
-#include "../generated/images/portraits/face065.h"
-#include "../generated/images/portraits/face066.h"
-#include "../generated/images/portraits/face067.h"
-#include "../generated/images/portraits/face068.h"
-#include "../generated/images/portraits/face069.h"
-#include "../generated/images/portraits/face070.h"
-#include "../generated/images/portraits/face071.h"
-#include "../generated/images/portraits/face072.h"
-#include "../generated/images/portraits/face073.h"
-#include "../generated/images/portraits/face074.h"
-#include "../generated/images/portraits/face075.h"
-#include "../generated/images/portraits/face076.h"
-#include "../generated/images/portraits/face077.h"
-#include "../generated/images/portraits/face078.h"
-#include "../generated/images/portraits/face079.h"
-#include "../generated/images/portraits/face080.h"
-#include "../generated/images/portraits/face081.h"
-#include "../generated/images/portraits/face082.h"
-#include "../generated/images/portraits/face083.h"
-#include "../generated/images/portraits/face084.h"
-#include "../generated/images/portraits/face085.h"
-#include "../generated/images/portraits/face086.h"
-#include "../generated/images/portraits/face087.h"
-#include "../generated/images/portraits/face088.h"
-#include "../generated/images/portraits/face089.h"
-#include "../generated/images/portraits/face090.h"
-#include "../generated/images/portraits/face091.h"
-#include "../generated/images/portraits/face092.h"
-#include "../generated/images/portraits/face093.h"
-#include "../generated/images/portraits/face094.h"
-#include "../generated/images/portraits/face095.h"
-#include "../generated/images/portraits/face096.h"
-#include "../generated/images/portraits/face097.h"
-#include "../generated/images/portraits/face098.h"
-#include "../generated/images/portraits/face099.h"
-#include "../generated/images/portraits/face100.h"
-#include "../generated/images/portraits/face101.h"
-#include "../generated/images/portraits/face102.h"
-#include "../generated/images/portraits/face103.h"
-#include "../generated/images/portraits/face104.h"
-#include "../generated/images/portraits/face105.h"
-#include "../generated/images/portraits/face106.h"
-#include "../generated/images/portraits/face107.h"
-#include "../generated/images/portraits/face108.h"
-#include "../generated/images/portraits/face109.h"
-#include "../generated/images/portraits/face110.h"
-#include "../generated/images/portraits/face111.h"
-#include "../generated/images/portraits/face112.h"
-#include "../generated/images/portraits/face113.h"
-#include "../generated/images/portraits/face114.h"
-#include "../generated/images/portraits/face115.h"
-#include "../generated/images/portraits/face116.h"
-#include "../generated/images/portraits/face117.h"
-#include "../generated/images/portraits/face118.h"
-#include "../generated/images/portraits/face119.h"
-#include "../generated/images/portraits/face120.h"
-#include "../generated/images/portraits/face121.h"
-#include "../generated/images/portraits/face122.h"
-#include "../generated/images/portraits/face123.h"
-#include "../generated/images/portraits/face124.h"
-#include "../generated/images/portraits/face125.h"
-#include "../generated/images/portraits/face126.h"
-#include "../generated/images/portraits/face127.h"
-#include "../generated/images/portraits/face128.h"
-#include "../generated/images/portraits/face129.h"
-#include "../generated/images/portraits/face130.h"
-#include "../generated/images/portraits/face131.h"
-#include "../generated/images/portraits/face132.h"
-#include "../generated/images/portraits/face133.h"
-#include "../generated/images/portraits/face134.h"
-#include "../generated/images/portraits/face135.h"
-#include "../generated/images/portraits/face136.h"
-#include "../generated/images/portraits/face137.h"
-#include "../generated/images/portraits/face138.h"
-#include "../generated/images/portraits/face139.h"
-#include "../generated/images/portraits/face140.h"
-#include "../generated/images/portraits/face141.h"
-#include "../generated/images/portraits/face142.h"
-#include "../generated/images/portraits/face143.h"
-#include "../generated/images/portraits/face144.h"
-#include "../generated/images/portraits/face145.h"
-#include "../generated/images/portraits/face146.h"
-#include "../generated/images/portraits/face147.h"
-#include "../generated/images/portraits/face148.h"
-#include "../generated/images/portraits/face149.h"
-#include "../generated/images/portraits/face150.h"
-#include "../generated/images/portraits/face151.h"
-#include "../generated/images/portraits/face152.h"
-#include "../generated/images/portraits/face153.h"
-#include "../generated/images/portraits/face154.h"
-#include "../generated/images/portraits/face155.h"
-#include "../generated/images/portraits/face156.h"
-#include "../generated/images/portraits/face157.h"
-#include "../generated/images/portraits/face158.h"
-#include "../generated/images/portraits/face159.h"
-#include "../generated/images/portraits/face160.h"
-#include "../generated/images/portraits/face161.h"
-#include "../generated/images/portraits/face162.h"
-#include "../generated/images/portraits/face163.h"
-#include "../generated/images/portraits/face164.h"
-#include "../generated/images/portraits/face165.h"
-#include "../generated/images/portraits/face166.h"
-#include "../generated/images/portraits/face167.h"
-#include "../generated/images/portraits/face168.h"
-#include "../generated/images/portraits/face169.h"
 
 
 u8* portraits_tiles[] = {
@@ -185,39 +55,7 @@ u8* portraits_tiles[] = {
 	(u8*)&face028Tiles, (u8*)&face029Tiles, (u8*)&face030Tiles, (u8*)&face031Tiles,
 	(u8*)&face032Tiles, (u8*)&face033Tiles, (u8*)&face034Tiles, (u8*)&face035Tiles,
 	(u8*)&face036Tiles, (u8*)&face037Tiles, (u8*)&face038Tiles, (u8*)&face039Tiles,
-	(u8*)&face040Tiles, (u8*)&face041Tiles, (u8*)&face042Tiles, (u8*)&face043Tiles,
-	(u8*)&face044Tiles, (u8*)&face045Tiles, (u8*)&face046Tiles, (u8*)&face047Tiles,
-	(u8*)&face048Tiles, (u8*)&face049Tiles, (u8*)&face050Tiles, (u8*)&face051Tiles,
-	(u8*)&face052Tiles, (u8*)&face053Tiles, (u8*)&face054Tiles, (u8*)&face055Tiles,
-	(u8*)&face056Tiles, (u8*)&face057Tiles, (u8*)&face058Tiles, (u8*)&face059Tiles,
-	(u8*)&face060Tiles, (u8*)&face061Tiles, (u8*)&face062Tiles, (u8*)&face063Tiles,
-	(u8*)&face064Tiles, (u8*)&face065Tiles, (u8*)&face066Tiles, (u8*)&face067Tiles,
-	(u8*)&face068Tiles, (u8*)&face069Tiles, (u8*)&face070Tiles, (u8*)&face071Tiles,
-	(u8*)&face072Tiles, (u8*)&face073Tiles, (u8*)&face074Tiles, (u8*)&face075Tiles,
-	(u8*)&face076Tiles, (u8*)&face077Tiles, (u8*)&face078Tiles, (u8*)&face079Tiles,
-	(u8*)&face080Tiles, (u8*)&face081Tiles, (u8*)&face082Tiles, (u8*)&face083Tiles,
-	(u8*)&face084Tiles, (u8*)&face085Tiles, (u8*)&face086Tiles, (u8*)&face087Tiles,
-	(u8*)&face088Tiles, (u8*)&face089Tiles, (u8*)&face090Tiles, (u8*)&face091Tiles,
-	(u8*)&face092Tiles, (u8*)&face093Tiles, (u8*)&face094Tiles, (u8*)&face095Tiles,
-	(u8*)&face096Tiles, (u8*)&face097Tiles, (u8*)&face098Tiles, (u8*)&face099Tiles,
-	(u8*)&face100Tiles, (u8*)&face101Tiles, (u8*)&face102Tiles, (u8*)&face103Tiles,
-	(u8*)&face104Tiles, (u8*)&face105Tiles, (u8*)&face106Tiles, (u8*)&face107Tiles,
-	(u8*)&face108Tiles, (u8*)&face109Tiles, (u8*)&face110Tiles, (u8*)&face111Tiles,
-	(u8*)&face112Tiles, (u8*)&face113Tiles, (u8*)&face114Tiles, (u8*)&face115Tiles,
-	(u8*)&face116Tiles, (u8*)&face117Tiles, (u8*)&face118Tiles, (u8*)&face119Tiles,
-	(u8*)&face120Tiles, (u8*)&face121Tiles, (u8*)&face122Tiles, (u8*)&face123Tiles,
-	(u8*)&face124Tiles, (u8*)&face125Tiles, (u8*)&face126Tiles, (u8*)&face127Tiles,
-	(u8*)&face128Tiles, (u8*)&face129Tiles, (u8*)&face130Tiles, (u8*)&face131Tiles,
-	(u8*)&face132Tiles, (u8*)&face133Tiles, (u8*)&face134Tiles, (u8*)&face135Tiles,
-	(u8*)&face136Tiles, (u8*)&face137Tiles, (u8*)&face138Tiles, (u8*)&face139Tiles,
-	(u8*)&face140Tiles, (u8*)&face141Tiles, (u8*)&face142Tiles, (u8*)&face143Tiles,
-	(u8*)&face144Tiles, (u8*)&face145Tiles, (u8*)&face146Tiles, (u8*)&face147Tiles,
-	(u8*)&face148Tiles, (u8*)&face149Tiles, (u8*)&face150Tiles, (u8*)&face151Tiles,
-	(u8*)&face152Tiles, (u8*)&face153Tiles, (u8*)&face154Tiles, (u8*)&face155Tiles,
-	(u8*)&face156Tiles, (u8*)&face157Tiles, (u8*)&face158Tiles, (u8*)&face159Tiles,
-	(u8*)&face160Tiles, (u8*)&face161Tiles, (u8*)&face162Tiles, (u8*)&face163Tiles,
-	(u8*)&face164Tiles, (u8*)&face165Tiles, (u8*)&face166Tiles, (u8*)&face167Tiles,
-	(u8*)&face168Tiles, (u8*)&face169Tiles, 
+	
 };
 
 
@@ -233,39 +71,7 @@ u8* portraits_pals[] = {
 	(u8*)&face028Pal, (u8*)&face029Pal, (u8*)&face030Pal, (u8*)&face031Pal,
 	(u8*)&face032Pal, (u8*)&face033Pal, (u8*)&face034Pal, (u8*)&face035Pal,
 	(u8*)&face036Pal, (u8*)&face037Pal, (u8*)&face038Pal, (u8*)&face039Pal,
-	(u8*)&face040Pal, (u8*)&face041Pal, (u8*)&face042Pal, (u8*)&face043Pal,
-	(u8*)&face044Pal, (u8*)&face045Pal, (u8*)&face046Pal, (u8*)&face047Pal,
-	(u8*)&face048Pal, (u8*)&face049Pal, (u8*)&face050Pal, (u8*)&face051Pal,
-	(u8*)&face052Pal, (u8*)&face053Pal, (u8*)&face054Pal, (u8*)&face055Pal,
-	(u8*)&face056Pal, (u8*)&face057Pal, (u8*)&face058Pal, (u8*)&face059Pal,
-	(u8*)&face060Pal, (u8*)&face061Pal, (u8*)&face062Pal, (u8*)&face063Pal,
-	(u8*)&face064Pal, (u8*)&face065Pal, (u8*)&face066Pal, (u8*)&face067Pal,
-	(u8*)&face068Pal, (u8*)&face069Pal, (u8*)&face070Pal, (u8*)&face071Pal,
-	(u8*)&face072Pal, (u8*)&face073Pal, (u8*)&face074Pal, (u8*)&face075Pal,
-	(u8*)&face076Pal, (u8*)&face077Pal, (u8*)&face078Pal, (u8*)&face079Pal,
-	(u8*)&face080Pal, (u8*)&face081Pal, (u8*)&face082Pal, (u8*)&face083Pal,
-	(u8*)&face084Pal, (u8*)&face085Pal, (u8*)&face086Pal, (u8*)&face087Pal,
-	(u8*)&face088Pal, (u8*)&face089Pal, (u8*)&face090Pal, (u8*)&face091Pal,
-	(u8*)&face092Pal, (u8*)&face093Pal, (u8*)&face094Pal, (u8*)&face095Pal,
-	(u8*)&face096Pal, (u8*)&face097Pal, (u8*)&face098Pal, (u8*)&face099Pal,
-	(u8*)&face100Pal, (u8*)&face101Pal, (u8*)&face102Pal, (u8*)&face103Pal,
-	(u8*)&face104Pal, (u8*)&face105Pal, (u8*)&face106Pal, (u8*)&face107Pal,
-	(u8*)&face108Pal, (u8*)&face109Pal, (u8*)&face110Pal, (u8*)&face111Pal,
-	(u8*)&face112Pal, (u8*)&face113Pal, (u8*)&face114Pal, (u8*)&face115Pal,
-	(u8*)&face116Pal, (u8*)&face117Pal, (u8*)&face118Pal, (u8*)&face119Pal,
-	(u8*)&face120Pal, (u8*)&face121Pal, (u8*)&face122Pal, (u8*)&face123Pal,
-	(u8*)&face124Pal, (u8*)&face125Pal, (u8*)&face126Pal, (u8*)&face127Pal,
-	(u8*)&face128Pal, (u8*)&face129Pal, (u8*)&face130Pal, (u8*)&face131Pal,
-	(u8*)&face132Pal, (u8*)&face133Pal, (u8*)&face134Pal, (u8*)&face135Pal,
-	(u8*)&face136Pal, (u8*)&face137Pal, (u8*)&face138Pal, (u8*)&face139Pal,
-	(u8*)&face140Pal, (u8*)&face141Pal, (u8*)&face142Pal, (u8*)&face143Pal,
-	(u8*)&face144Pal, (u8*)&face145Pal, (u8*)&face146Pal, (u8*)&face147Pal,
-	(u8*)&face148Pal, (u8*)&face149Pal, (u8*)&face150Pal, (u8*)&face151Pal,
-	(u8*)&face152Pal, (u8*)&face153Pal, (u8*)&face154Pal, (u8*)&face155Pal,
-	(u8*)&face156Pal, (u8*)&face157Pal, (u8*)&face158Pal, (u8*)&face159Pal,
-	(u8*)&face160Pal, (u8*)&face161Pal, (u8*)&face162Pal, (u8*)&face163Pal,
-	(u8*)&face164Pal, (u8*)&face165Pal, (u8*)&face166Pal, (u8*)&face167Pal,
-	(u8*)&face168Pal, (u8*)&face169Pal, 
+	
 };
 
 
