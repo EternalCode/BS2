849b8b6ee9f0adfd43912edab5bd8018ff4b583a
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-11-27 13:47:59 -0500

    Added state callbacks to world state

--- src/battle_state.h
+++ src/battle_state.h
@@ -7,10 +7,13 @@ union map_state_u {
     u8 map_2d[26][38]; // 23 rows and 35 coloums
     u8 map_1d[26 * 38];
 };
+
 struct engine_state {
     union map_state_u* map;
-    SuperCallback FSM_state;
-    
+    u8 objids_on_map[6];
+    // engine element positions
+    SuperCallback FSM_Player_state;
+    SuperCallback FSM_AI_state;   
 };
 
 static struct engine_state* world_state = (struct engine_state*)0x02023BE4;
