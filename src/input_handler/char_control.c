cf635e6e65a9359b6a3749c33429135c786b8b9b
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2016-12-12 19:23:10 -0500

    Modularity for input handler

--- src/input_handler/char_control.c
+++ src/input_handler/char_control.c
@@ -2,149 +2,102 @@
 #include "../battler_data/battler_data.h"
 #include "../battler_data/pokemon_base.h"
 
-void obj_priv_clear(struct Object* obj) {
-    obj->priv[7] = 0;
-    obj->priv[5] = 0;
-    obj->priv[6] = 0;
-}
+extern void obj_priv_clear(struct Object* obj);
+extern void protect_cb(struct Object* obj, struct battler* fighter);
 
-void process_input_player(u16 button_press) {
-    struct Object* obj = &objects[player->obj_id];
-    obj->priv[4] = 1;
-    // strip unneeded buttons for movements
-    u16 button = button_press & (KEY_A | KEY_B | KEY_L | KEY_R);
-    u16 species = player->species;
+void process_input(u16 button_press, struct battler* fighter) {
+    struct Object* obj = &objects[fighter->obj_id];
+    u16 species = fighter->species;
     
-    // check attack used
+    // blocking check
+    if ((button_press == KEY_DOWN)  && (fighter->protect_pp)) {
+        fighter->bstate = BATTLER_ATTACKING;
+        obj->callback = p_base[species].protect;
+        obj_priv_clear(obj);
+
+        return;
+    }
+    // strip unneeded buttons for attacks
+    u16 button = button_press & (KEY_A | KEY_B | KEY_L | KEY_R);
+    
+    // check attack used. Priority over movement
     if (button == (KEY_A)) {
         obj->callback = p_base[species].atk[0];
-        player->bstate = BATTLER_ATTACKING;
+        fighter->bstate = BATTLER_ATTACKING;
         obj_priv_clear(obj);
         return;
     } else if (button == (KEY_B)) {
         obj->callback = p_base[species].atk[1];
-        player->bstate = BATTLER_ATTACKING;
+        fighter->bstate = BATTLER_ATTACKING;
         obj_priv_clear(obj);
         return;
     } else if (button == (KEY_L)) {
         obj->callback = p_base[species].atk[2];
-        player->bstate = BATTLER_ATTACKING;
+        fighter->bstate = BATTLER_ATTACKING;
         obj_priv_clear(obj);
         return;
     } else if (button == (KEY_R)) {
         obj->callback = p_base[species].atk[3];
-        player->bstate = BATTLER_ATTACKING;
+        fighter->bstate = BATTLER_ATTACKING;
         obj_priv_clear(obj);
         return;
     }
     
-    button = button_press & (KEY_UP | KEY_LEFT | KEY_RIGHT | KEY_DOWN);
+    // Vertical vector removal during falling down
+    if (fighter->has_jumped) {
+        button = button_press & (KEY_LEFT | KEY_RIGHT);
+    } else {
+        button = button_press & (KEY_UP | KEY_LEFT | KEY_RIGHT);
+    }
+    
     // process input movements
-    if (button == (KEY_UP | KEY_LEFT)) {
-        // Diagonal upleft
+    if ((button == (KEY_UP | KEY_LEFT)) && (!fighter->has_jumped)) {
+        // up left
+        fighter->dir = FACING_RIGHT;
+        fighter->bstate = BATTLER_UNCONTROLLABLE_JUMPING;
         obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_UL;
-        obj->final_oam.h_flip = 0;
-    } else if (button == (KEY_UP | KEY_RIGHT)) {
-        // Diagonal upright
+        obj->callback = p_base[species].jump_U;
+        fighter->apply_dir_to_jump = true;
+        fighter->delta_time = 0;
+    } else if ((button == (KEY_UP | KEY_RIGHT)) && (!fighter->has_jumped)) {
+        // up right
+        fighter->dir = FACING_LEFT;
+        fighter->bstate = BATTLER_UNCONTROLLABLE_JUMPING;
         obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_UR;
-        obj->final_oam.h_flip = 1;
-    } else if (button == (KEY_DOWN | KEY_LEFT)) {
-        // Diagonal downleft  
-        obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_DL;
-        obj->final_oam.h_flip = 0;
-    } else if (button == (KEY_DOWN | KEY_RIGHT)) {
-        // Diagonal downright
-        obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_DR;
-        obj->final_oam.h_flip = 1;
+        obj->callback = p_base[species].jump_U;
+        fighter->apply_dir_to_jump = true;
+        fighter->delta_time = 0;
     } else if (button == (KEY_LEFT)) {
         // left
-        obj_priv_clear(obj);
+        obj->priv[2] = (fighter->dir == FACING_RIGHT) ? obj->priv[2] : 0;
+        fighter->dir = FACING_RIGHT;
         obj->callback = p_base[species].walk_L;
-        obj->final_oam.h_flip = 0;
     } else if (button == (KEY_RIGHT)) {
         // right
-        obj_priv_clear(obj);
+        obj->priv[2] = (fighter->dir == FACING_LEFT) ? obj->priv[2] : 0;
+        fighter->dir = FACING_LEFT;
         obj->callback = p_base[species].walk_R;
-        obj->final_oam.h_flip = 1;
-    } else if (button == (KEY_UP)) {
+    } else if ((button == (KEY_UP)) && (!fighter->has_jumped)) {
         // up
+        fighter->bstate = BATTLER_UNCONTROLLABLE_JUMPING;
         obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_U;
-    } else if (button == (KEY_DOWN)) {
-        // down
-        obj_priv_clear(obj);
-        obj->callback = p_base[species].walk_D;
+        obj->callback = p_base[species].jump_U;
+        fighter->apply_dir_to_jump = false;
+        fighter->delta_time = 0;
+        
     } else {
-        obj_priv_clear(obj);
+       // obj_priv_clear(obj);
         // continue idle state
     }
 }
 
 
-void process_input_opponent(u16 button_press) {
-    // AI when it happens
-   struct Object* obj = &objects[opponent->obj_id];
-    obj->priv[4] = 0;
-    // strip unneeded buttons for movements
+void process_uturn(u16 button_press, struct battler* fighter) {
     u16 button = button_press & (KEY_A | KEY_B | KEY_L | KEY_R);
-    u16 species = opponent->species;
-    
-    // check attack used
-    if (button == (KEY_A)) {
-        obj->callback = p_base[species].atk[0];
-        opponent->bstate = BATTLER_ATTACKING;
-        return;
-    } else if (button == (KEY_B)) {
-        obj->callback = p_base[species].atk[1];
-        opponent->bstate = BATTLER_ATTACKING;
-        return;
-    } else if (button == (KEY_L)) {
-        obj->callback = p_base[species].atk[2];
-        opponent->bstate = BATTLER_ATTACKING;
-        return;
-    } else if (button == (KEY_R)) {
-        obj->callback = p_base[species].atk[3];
-        opponent->bstate = BATTLER_ATTACKING;
-        return;
+    if (button) {
+        //cancel state, enter
     }
-    
-    button = button_press & (KEY_UP | KEY_LEFT | KEY_RIGHT | KEY_DOWN);
-    // process input movements
-    if (button == (KEY_UP | KEY_LEFT)) {
-        // Diagonal upleft
-        obj->callback = p_base[species].walk_UL;
-        obj->final_oam.h_flip = 0;
-    } else if (button == (KEY_UP | KEY_RIGHT)) {
-        // Diagonal upright
-        obj->callback = p_base[species].walk_UR;
-        obj->final_oam.h_flip = 1;
-    } else if (button == (KEY_DOWN | KEY_LEFT)) {
-        // Diagonal downleft  
-        obj->callback = p_base[species].walk_DL;
-        obj->final_oam.h_flip = 0;
-    } else if (button == (KEY_DOWN | KEY_RIGHT)) {
-        // Diagonal downright
-        obj->callback = p_base[species].walk_DR;
-        obj->final_oam.h_flip = 1;
-    } else if (button == (KEY_LEFT)) {
-        // left
-        obj->callback = p_base[species].walk_L;
-        obj->final_oam.h_flip = 0;
-    } else if (button == (KEY_RIGHT)) {
-        // right
-        obj->callback = p_base[species].walk_R;
-        obj->final_oam.h_flip = 1;
-    } else if (button == (KEY_UP)) {
-        // up
-        obj->callback = p_base[species].walk_U;
-    } else if (button == (KEY_DOWN)) {
-        // down
-        obj->callback = p_base[species].walk_D;
-    } else {
-        // continue idle state
-    }
+    return;
 }
+
+
